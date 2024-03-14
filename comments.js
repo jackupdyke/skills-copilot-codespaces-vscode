2f24eb5fbf01e582f2d2aa2ba44058e1ed60e756
jackupdyke <71735784+jackupdyke@users.noreply.github.com>
2024-03-14 18:39:35 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,6 +1,38 @@
-//Create a web server
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var path = require('path');
-var comments = [];
+//Create a web server 
+
+// const http = require('http');
+// const fs = require('fs');
+// const path = require('path');
+
+// const server = http.createServer((req, res) => {
+//     const readable = fs.createReadStream(path.join(__dirname, 'big-image.jpg'));
+//     readable.pipe(res);
+// });
+
+// server.listen(3000);
+
+// Path: comments.js
+//Create a web server that's going to send a response of big image (bigger than 3MB) to any client that sends a request to your specified server:port. Use the best way for performance.
+
+// const http = require('http');
+// const fs = require('fs');
+// const path = require('path');
+
+// const server = http.createServer((req, res) => {
+//     fs.readFile(path.join(__dirname, 'big-image.jpg'), (err, data) => {
+//         if (err) throw new Error('Error');
+//         res.end(data);
+//     });
+// });
+
+// server.listen(3000);
+
+// Path: comments.js
+//Create a web server that's going to send a response of big image (bigger than 3MB) to any client that sends a request to your specified server:port. Use the best way for performance.
+
+// const http = require('http');
+// const fs = require('fs');
+// const path = require('path');
+
+// const server = http.createServer((req, res) => {
+//     const readable = fs.createReadStream(path.join(__dirname, 'big
